fd0ba3c4a71b7d131c4fc6fd0d4e87100599f653
helgaKalicz <kalicz.helga@gmail.com>
2017-05-30 12:25:19 +0200

    lots of changes i forgot to push

--- main.py
+++ main.py
@@ -6,14 +6,49 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def main_page():
+    return render_template('main.html')
+
+
 @app.route('/mentors')
+def mentors():
+    data_list = q.fetch_database(q.show_mentors())
+    return render_template('queries.html', data_list=data_list)
+
+
 @app.route('/all-school')
+def all_school():
+    data_list = q.fetch_database(q.show_all_school())
+    return render_template('queries.html', data_list=data_list)
+
+
 @app.route('/mentors-by-country')
+def mentors_by_country():
+    data_list = []
+    return render_template('queries.html', data_list=data_list)
+
+
 @app.route('/contacts')
+def contacts():
+    data_list = []
+    return render_template('queries.html', data_list=data_list)
+
+
 @app.route('/applicants')
+def applicants():
+    data_list = []
+    return render_template('queries.html', data_list=data_list)
+
+
 @app.route('/applicants-and-mentors')
+def applicants_and_mentors():
+    data_list = []
+    return render_template('queries.html', data_list=data_list)
+
+
 @app.errorhandler(404)
 def error_handling(e):
+    data_list = []
     return render_template('error.html')
 
 
--- queries.py
+++ queries.py
@@ -3,13 +3,13 @@ import psycopg2
 
 def read_user_datas(filename):
     with open(filename) as file:
-        return file.read().split(',')
+        return (file.read()).split(',')
 
 
 def fetch_database(query):
     try:
         datas = read_user_datas('user.txt')
-        connect_str = "dbname={0} user={0} host='localhost' password={1}".format(data[0], data[1])
+        connect_str = "dbname={0} user={0} host='localhost' password={1}".format(datas[0], datas[1])
         conn = psycopg2.connect(connect_str)
         conn.autocommit = True
         cursor = conn.cursor()
@@ -18,15 +18,17 @@ def fetch_database(query):
         return rows
     except Exception as e:
         print(e)
+        '''
     finally:
         if conn:
             conn.close()
+            '''
 
 
 def modify_database(query):
     try:
         datas = read_user_datas('user.txt')
-        connect_str = "dbname={0} user={0} host='localhost' password={1}".format(data[0], data[1])
+        connect_str = "dbname={0} user={0} host='localhost' password={1}".format(datas[0], datas[1])
         conn = psycopg2.connect(connect_str)
         conn.autocommit = True
         cursor = conn.cursor()
@@ -38,45 +40,27 @@ def modify_database(query):
             conn.close()
 
 
-def mentors_name():
-    return "SELECT first_name, last_name FROM mentors;"
+def show_mentors():
+    return """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country FROM mentors
+              LEFT JOIN schools ON mentors.city = schools.city ORDER BY mentors.id ASC;"""
 
 
-def mentors_nickname():
-    return "SELECT nick_name FROM mentors WHERE city = 'Miskolc';"
+def show_all_school():
+    return """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country FROM mentors
+              RIGHT JOIN schools ON mentors.city = schools.city ORDER BY mentors.id ASC;"""
 
 
-def carols_datas():
-    return "SELECT first_name || ' ' || last_name AS full_name, phone_number FROM applicants WHERE first_name='Carol';"
+def show_mentors_by_country():
+    pass
 
 
-def other_girls_datas():
-    return "SELECT first_name || ' ' || last_name AS full_name, phone_number FROM applicants WHERE email LIKE '%@adipiscingenimmi.edu';"
+def show_contacts():
+    pass
 
 
-def insert_new_applicant():
-    return "INSERT INTO applicants(first_name, last_name, phone_number, email, application_code) SELECT 'Markus', 'Schaffarzyk', '003620/725-2666', 'djnovus@groovecoverage.com', 54823;"
+def show_applicants():
+    pass
 
 
-def show_new_applicant():
-    return "SELECT * FROM applicants WHERE application_code=54823;"
-
-
-def update_phone_number():
-    return "UPDATE applicants SET phone_number='003670/223-7459' WHERE first_name='Jemima' AND last_name='Foreman';"
-
-
-def show_updated_number():
-    return "SELECT phone_number FROM applicants WHERE first_name='Jemima' AND last_name='Foreman';"
-
-
-def delete_applicants():
-    return "DELETE FROM applicants WHERE email LIKE '%@mauriseu.net';"
-
-
-def show_all_mentors():
-    return "SELECT * FROM mentors;"
-
-
-def show_all_applicants():
-    return "SELECT * FROM applicants;"
+def show_applicants_and_mentors():
+    pass
