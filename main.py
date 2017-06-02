82e4c814817a16d51f8dbeea3d0a65d0c8a7590d
helgaKalicz <kalicz.helga@gmail.com>
2017-06-02 13:42:37 +0200

    table-head added and so on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, redirect
 import psycopg2
-import queries as q
+import queries
 
 app = Flask(__name__)
 
@@ -12,43 +12,48 @@ def main_page():
 
 @app.route('/mentors')
 def mentors():
-    data_list = q.fetch_database(q.show_mentors())
-    return render_template('queries.html', data_list=data_list)
+    return render_template('queries.html',
+                           data_list=queries.fetch_database(queries.show_mentors()),
+                           table_head=['Mentor first name', 'Mentor last name', 'School name', 'Country'])
 
 
 @app.route('/all-school')
 def all_school():
-    data_list = q.fetch_database(q.show_all_school())
-    return render_template('queries.html', data_list=data_list)
+    return render_template('queries.html',
+                           data_list=queries.fetch_database(queries.show_all_school()),
+                           table_head=['Mentor first name', 'Mentor last name', 'School name', 'Country'])
 
 
 @app.route('/mentors-by-country')
 def mentors_by_country():
-    data_list = q.fetch_database(q.show_mentors_by_country())
-    return render_template('queries.html', data_list=data_list)
+    return render_template('queries.html',
+                           data_list=queries.fetch_database(queries.show_mentors_by_country()),
+                           table_head=['Country', 'Mentor'])
 
 
 @app.route('/contacts')
 def contacts():
-    data_list = q.fetch_database(q.show_contacts())
-    return render_template('queries.html', data_list=data_list)
+    return render_template('queries.html',
+                           data_list=queries.fetch_database(queries.show_contacts()),
+                           table_head=['School name', 'Mentor first name', 'Mentor last name'])
 
 
 @app.route('/applicants')
 def applicants():
-    data_list = []
-    return render_template('queries.html', data_list=data_list)
+    return render_template('queries.html',
+                           data_list=queries.fetch_database(queries.show_applicants()),
+                           table_head=['Applicant first name', 'Application code', 'Creation_date'])
 
 
 @app.route('/applicants-and-mentors')
 def applicants_and_mentors():
-    data_list = []
-    return render_template('queries.html', data_list=data_list)
+    return render_template('queries.html',
+                           data_list=queries.fetch_database(queries.show_applicants_and_mentors()),
+                           table_head=['Applicants first name', 'Application code', 'Mentor first name', 'Mentor last name'])
 
 
 @app.errorhandler(404)
 def error_handling(e):
-    data_list = []
     return render_template('error.html')
 
 
